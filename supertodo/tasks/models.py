98623a896de119105de88f5e80ec4703a55ce6d4
Cristian Reyes Hernández <cristianreyesh01@gmail.com>
2025-10-10 19:08:41 +0100

    model and core tests realized

--- supertodo/tasks/models.py
+++ supertodo/tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True, max_length=128)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
