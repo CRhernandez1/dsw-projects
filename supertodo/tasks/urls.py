ee9533d5d1ca8983c04943ceb84d9dfe092dab68
Cristian Reyes Hernández <cristianreyesh01@gmail.com>
2025-10-11 22:28:07 +0100

    1 test remaining

--- supertodo/tasks/urls.py
+++ supertodo/tasks/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('', views.task_list, name='task-list'),
     path('pending/', views.task_list_pending, name='task-list-pending'),
     path('completed/', views.task_list_completed, name='task-list-completed'),
+    path('add/', views.add_task, name='add-task'),
+    path('<slug:task_slug>/', views.task_detail, name='task-detail'),
+    path('<slug:task_slug>/delete/', views.delete_task, name='delete-task'),
+    path('<slug:task_slug>/edit/', views.edit_task, name='edit-task'),
+    path('<slug:task_slug>/toggle/', views.toggle_task, name='toggle-task'),
 ]
--- supertodo/tasks/views.py
+++ supertodo/tasks/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.utils.text import slugify
 
+from .forms import AddTaskForm, EditTaskForm
 from .models import Task
 
 
@@ -16,3 +19,49 @@ def task_list_pending(request):
 def task_list_completed(request):
     tasks = Task.objects.filter(completed=True)
     return render(request, 'tasks/task/list.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if (form := AddTaskForm(request.POST or None)).is_valid():
+        task = form.save(commit=False)
+        task.slug = slugify(task.name)
+        task.save()
+        return redirect('tasks:task-list')
+    return render(request, 'tasks/task/add.html', dict(form=form))
+
+
+def task_detail(request, task_slug: str):
+    try:
+        task = Task.objects.get(slug=task_slug)
+    except Task.DoesNotExist:
+        return HttpResponse(f'La tarea con el slug {task_slug} no existe!')
+    return render(request, 'tasks/task/detail.html', {'task': task})
+
+
+def delete_task(request, task_slug: str):
+    try:
+        task = Task.objects.get(slug=task_slug)
+    except Task.DoesNotExist:
+        return HttpResponse(f'La tarea con el slug {task_slug} no existe!')
+    task.delete()
+    return redirect('tasks:task-list')
+
+
+def edit_task(request, task_slug: str):
+    task = Task.objects.get(slug=task_slug)
+    if (form := EditTaskForm(request.POST or None, instance=task)).is_valid():
+        task = form.save(commit=False)
+        task.slug = slugify(task)
+        task.save()
+        return redirect('tasks:task-detail', task_slug=task.slug)
+    return render(request, 'tasks/task/add.html', dict(task=task, form=form))
+
+
+def toggle_task(request, task_slug: str):
+    try:
+        task = Task.objects.get(slug=task_slug)
+    except Task.DoesNotExist:
+        return HttpResponse(f'La tarea con el slug {task_slug} no existe!')
+    task.completed = not task.completed
+    task.save()
+    return redirect('tasks:task-list')
